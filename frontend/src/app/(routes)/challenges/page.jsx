7528387e72414caaffc5f75b95cd0cc5dd328b23
Pedraza Joaquin <joaquin.n.pedraza@gmail.com>
2024-02-19 11:42:11 -0300

    up

---
+++ frontend/src/app/(routes)/challenges/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const pageChallenges = () => {
+  return <div>pageChallenges</div>
+}
+
+export default pageChallenges
---
+++ frontend/src/components/buttons/Languages.jsx
@@ -0,0 +1,50 @@
+"use client";
+import React from "react";
+import { useState, useEffect } from "react";
+import { Button, styled } from "@mui/material";
+import Stack from "@mui/material/Stack";
+
+const LanguageButton = styled(Button)({
+  color: "#ffffff",
+  borderColor: "#86198f",
+  "&:hover": {
+    borderColor: "#d946ef",
+    backgroundColor: "#d946ef",
+  },
+});
+
+const Languages = ({ onClick, data }) => {
+  const [selectedLanguage, setSelectedLanguage] = useState(
+    Object.keys(data)[0]
+  );
+
+  const handleButtonClick = (language) => {
+    setSelectedLanguage(language);
+    onClick(language);
+  };
+
+  useEffect(() => {
+    onClick(selectedLanguage);
+  }, [selectedLanguage, onClick]);
+
+  return (
+    <div>
+      <Stack spacing={2} direction="row">
+        {Object.keys(data).map((language, index) => (
+          <LanguageButton
+            key={index}
+            variant="outlined"
+            onClick={() => handleButtonClick(language)}
+            style={{
+              backgroundColor: selectedLanguage === language ? "#d946ef" : "",
+            }}
+          >
+            {language}
+          </LanguageButton>
+        ))}
+      </Stack>
+    </div>
+  );
+};
+
+export default Languages;
